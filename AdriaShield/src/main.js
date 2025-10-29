7575c3eb0b08ef72d990fa0e69da29028fcd0da6
Tim Pirotte <timpirotte@outlook.com>
2025-10-29 22:25:42 +0100

    Removes redundant router folder

--- AdriaShield/src/main.js
+++ AdriaShield/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';
+import router from './router.js';
 
 import './assets/styles/base.css';
 import './assets/styles/reset.css';
--- AdriaShield/src/router.js
+++ AdriaShield/src/router.js
@@ -1,16 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import AreaInfoPage from '../pages/AreaInfoPage.vue';
-import AuthPage from '../pages/AuthPage.vue';
-import ConfirmPlanPage from '../pages/ConfirmPlanPage.vue';
-import FamilyPage from '../pages/FamilyPage.vue';
-import FollowingPage from '../pages/FollowingPage.vue';
-import MapPage from '../pages/MapPage.vue';
-import NotificationsPage from '../pages/NotificationsPage.vue';
-import PlanPage from '../pages/PlanPage.vue';
-import SettingsPage from '../pages/SettingsPage.vue';
-import ShelterInfoPage from '../pages/ShelterInfoPage.vue';
-import ThankYouPage from '../pages/ThankYouPage.vue';
+import AreaInfoPage from './pages/AreaInfoPage.vue';
+import AuthPage from './pages/AuthPage.vue';
+import ConfirmPlanPage from './pages/ConfirmPlanPage.vue';
+import FamilyPage from './pages/FamilyPage.vue';
+import FollowingPage from './pages/FollowingPage.vue';
+import MapPage from './pages/MapPage.vue';
+import NotificationsPage from './pages/NotificationsPage.vue';
+import PlanPage from './pages/PlanPage.vue';
+import SettingsPage from './pages/SettingsPage.vue';
+import ShelterInfoPage from './pages/ShelterInfoPage.vue';
+import ThankYouPage from './pages/ThankYouPage.vue';
 
 const routes = [
   { path: '/', component: AuthPage },
