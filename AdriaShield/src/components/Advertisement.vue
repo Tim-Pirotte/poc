63274ce0e46b5d93661eb0f951a94e11cd0696d2
Tim Pirotte <timpirotte@outlook.com>
2025-11-02 14:42:14 +0100

    Adds the html for the advertisement

---
+++ AdriaShield/src/components/Advertisement.vue
@@ -0,0 +1,18 @@
+<script setup>
+import ProgressiveImage from './icons/ProgressiveImage.vue';
+
+import advertisement from '../mock-data/advertisement.json';
+import plan from '../mock-data/plan.json';
+</script>
+
+<template>
+    <aside v-if="plan.showAds" aria-label="Advertisement">
+        <router-link to="/plans">Remove</router-link>
+        <a :href="advertisement.url" target="_blank" rel="noopener sponsored">
+            <ProgressiveImage :sources="advertisement.sources" :alt="advertisement.alt"/>
+        </a>
+    </aside>
+</template>
+
+<style scoped>
+</style>
--- AdriaShield/src/pages/MapPage.vue
+++ AdriaShield/src/pages/MapPage.vue
@@ -1,11 +1,13 @@
 <script setup>
 import HeaderBar from '@/components/HeaderBar.vue';
+import Advertisement from '@/components/Advertisement.vue';
 import FooterBar from '@/components/FooterBar.vue';
 </script>
 
 <template>
   <HeaderBar/>
   <h1>MapPage</h1>
+  <Advertisement/>
   <FooterBar/>
 </template>
 
